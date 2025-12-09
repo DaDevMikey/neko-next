cce115be1d085f9cf819f346bfeeb231229e6d1c
DaManMikey <169651161+DaDevMikey@users.noreply.github.com>
2025-12-09 09:53:57 +0100

    Implement system tray with speed and size controls
    Added system tray functionality with speed and size options.

--- main.go
+++ main.go
@@ -9,9 +9,11 @@ import (
 	"io/fs"
 	"log"
 	"math"
+	"os"
 	"path/filepath"
 	"strings"
 
+	"github.com/getlantern/systray"
 	"github.com/hajimehoshi/ebiten/v2"
 
 	"github.com/hajimehoshi/ebiten/v2/audio"
@@ -84,8 +86,7 @@ func (m *neko) Update() error {
 	if m.state == 10 && m.count == m.min {
 		playSound(mSound["idle3"])
 	}
-	// Prevents neko from being stuck on the side of the screen
-	// or randomly travelling to another monitor
+	
 	m.x = max(0, min(m.x, float64(monitorWidth)))
 	m.y = max(0, min(m.y, float64(monitorHeight)))
 	ebiten.SetWindowPosition(int(math.Round(m.x)), int(math.Round(m.y)))
@@ -119,26 +120,20 @@ func (m *neko) Update() error {
 }
 
 func (m *neko) stayIdle() {
-	// idle state
 	switch m.state {
 	case 0:
 		m.state = 1
 		fallthrough
-
 	case 1, 2, 3:
 		m.sprite = "awake"
-
 	case 4, 5, 6:
 		m.sprite = "scratch"
-
 	case 7, 8, 9:
 		m.sprite = "wash"
-
 	case 10, 11, 12:
 		m.min = 32
 		m.max = 64
 		m.sprite = "yawn"
-
 	default:
 		m.sprite = "sleep"
 	}
@@ -149,36 +144,35 @@ func (m *neko) catchCursor(x, y int) {
 	m.min = 8
 	m.max = 16
 
-	// get mouse direction
 	r := math.Atan2(float64(y), float64(x))
-	a := math.Mod((r/math.Pi*180)+360, 360) // Normazing angle to [0, 360)
+	a := math.Mod((r/math.Pi*180)+360, 360) 
 
 	switch {
-	case a <= 292.5 && a > 247.5: // up
+	case a <= 292.5 && a > 247.5: 
 		m.y -= cfg.Speed
 		m.sprite = "up"
-	case a <= 337.5 && a > 292.5: // up right
+	case a <= 337.5 && a > 292.5: 
 		m.x += cfg.Speed / math.Sqrt2
 		m.y -= cfg.Speed / math.Sqrt2
 		m.sprite = "upright"
-	case a <= 22.5 || a > 337.5: // right
+	case a <= 22.5 || a > 337.5: 
 		m.x += cfg.Speed
 		m.sprite = "right"
-	case a <= 67.5 && a > 22.5: // down right
+	case a <= 67.5 && a > 22.5: 
 		m.x += cfg.Speed / math.Sqrt2
 		m.y += cfg.Speed / math.Sqrt2
 		m.sprite = "downright"
-	case a <= 112.5 && a > 67.5: // down
+	case a <= 112.5 && a > 67.5: 
 		m.y += cfg.Speed
 		m.sprite = "down"
-	case a <= 157.5 && a > 112.5: // down left
+	case a <= 157.5 && a > 112.5: 
 		m.x -= cfg.Speed / math.Sqrt2
 		m.y += cfg.Speed / math.Sqrt2
 		m.sprite = "downleft"
-	case a <= 202.5 && a > 157.5: // left
+	case a <= 202.5 && a > 157.5: 
 		m.x -= cfg.Speed
 		m.sprite = "left"
-	case a <= 247.5 && a > 202.5: // up left
+	case a <= 247.5 && a > 202.5: 
 		m.x -= cfg.Speed
 		m.y -= cfg.Speed
 		m.sprite = "upleft"
@@ -200,7 +194,6 @@ func (m *neko) Draw(screen *ebiten.Image) {
 
 	if m.count > m.max {
 		m.count = 0
-
 		if m.state > 0 {
 			m.state++
 			switch m.state {
@@ -215,16 +208,73 @@ func (m *neko) Draw(screen *ebiten.Image) {
 	}
 
 	m.lastSprite = sprite
-
 	screen.Clear()
-
 	screen.DrawImage(m.img, nil)
+}
+
+// System Tray Logic
+func onReady() {
+	// Icon laden uit assets
+	iconData, _ := f.ReadFile("assets/awake.png")
+	systray.SetIcon(iconData)
+	systray.SetTitle("Neko")
+	systray.SetTooltip("Neko - The Cat")
+
+	// Menu items
+	mSpeed := systray.AddMenuItem("Snelheid", "Pas snelheid aan")
+	mSpeedSlow := mSpeed.AddSubMenuItem("Langzaam", "Slow speed")
+	mSpeedNormal := mSpeed.AddSubMenuItem("Normaal", "Normal speed")
+	mSpeedFast := mSpeed.AddSubMenuItem("Zoomies!", "Fast speed")
+
+	mScale := systray.AddMenuItem("Grootte", "Pas grootte aan")
+	mScaleSmall := mScale.AddSubMenuItem("Klein (1x)", "Small")
+	mScaleNormal := mScale.AddSubMenuItem("Normaal (2x)", "Normal")
+	mScaleBig := mScale.AddSubMenuItem("Groot (3x)", "Big")
+
+	systray.AddSeparator()
+	mQuit := systray.AddMenuItem("Afsluiten", "Bye bye Neko")
+
+	// Handle clicks in een loop
+	go func() {
+		for {
+			select {
+			case <-mQuit.ClickedCh:
+				systray.Quit()
+			case <-mSpeedSlow.ClickedCh:
+				cfg.Speed = 1.0
+			case <-mSpeedNormal.ClickedCh:
+				cfg.Speed = 2.0
+			case <-mSpeedFast.ClickedCh:
+				cfg.Speed = 4.0
+			case <-mScaleSmall.ClickedCh:
+				updateScale(1.0)
+			case <-mScaleNormal.ClickedCh:
+				updateScale(2.0)
+			case <-mScaleBig.ClickedCh:
+				updateScale(3.0)
+			}
+		}
+	}()
+}
+
+func updateScale(s float64) {
+	cfg.Scale = s
+	ebiten.SetWindowSize(int(float64(width)*s), int(float64(height)*s))
+}
+
+func onExit() {
+	os.Exit(0)
 }
 
 func main() {
 	config.PrefixEnv = "NEKO"
 	config.File = "neko.ini"
 	config.Parse(cfg)
+
+	// Start systray in een aparte goroutine (werkt het best op Windows)
+	go func() {
+		systray.Run(onReady, onExit)
+	}()
 
 	mSprite = make(map[string]*ebiten.Image)
 	mSound = make(map[string][]byte)
@@ -242,7 +292,6 @@ func main() {
 			if err != nil {
 				log.Fatal(err)
 			}
-
 			mSprite[name] = ebiten.NewImageFromImage(img)
 		case ".wav":
 			stream, err := wav.DecodeWithSampleRate(44100, bytes.NewReader(data))
@@ -253,15 +302,11 @@ func main() {
 			if err != nil {
 				log.Fatal(err)
 			}
-
 			mSound[name] = data
 		}
 	}
 
 	audio.NewContext(44100)
-
-	// Workaround: for some reason playing the first sound can incur significant delay.
-	// So let's do this at the start.
 	audio.CurrentContext().NewPlayerFromBytes([]byte{}).Play()
 
 	n := &neko{
